64e264aaa3131b1169d5d84ba9cd88f8c5805f40
Gabriel Valdez <GabrielVCI>
2022-07-15 22:01:51 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Multitarea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Thread t = new Thread(MetodoContador);
+            t.Start(); //Sirve para iniciar la tarea simultanea.
+
+            Console.WriteLine("Probando los thread.");
+            Thread.Sleep(500);
+
+        }
+
+        static void MetodoContador()
+        {
+            int contador = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Incrementando el contador " + contador);
+                contador++;
+            }
+            Thread.Sleep(500);
+
+            Console.WriteLine("Probando los thread en el contador.");
+            Thread.Sleep(500);
+        }
+    }
+}
